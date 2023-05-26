a9d32641015256b4220f53c5dd4ef1321014314e
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-26 14:04:08 +0700

    run migrations

---
+++ sequelize.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        database: 'todo_skilvul',
+        password: 'toor',
+        username: 'root',
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
+    },
+};
