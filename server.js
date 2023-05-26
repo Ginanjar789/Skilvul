786b0466f241b553df32a8076c353bbd6756a741
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-26 14:12:19 +0700

    add endpoint list

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('ok');
-});
+app.post('/register', async function(req, res) {});
+app.post('/login', async function(req, res) {});
+app.post('/todos', async function(req, res) {});
+app.get('/todos', async function(req, res) {});
+app.get('/todos/:id', async function(req, res) {});
+app.put('/todos/:id', async function(req, res) {});
+app.delete('/todos/:id', async function(req, res) {});
+app.delete('/todos', async function(req, res) {});
 
 app.listen(3000);
